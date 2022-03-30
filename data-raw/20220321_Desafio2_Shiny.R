54e8044d2a5ed37bbfe95de44105cbc3cf235b3a
Maykon G. Pedro <maykonglaffite@gmail.com>
2022-03-29 23:52:45 -0300

    add time and cont

--- data-raw/20220321_Desafio2_Shiny.R
+++ data-raw/20220321_Desafio2_Shiny.R
@@ -1,7 +1,14 @@
 
 library(shiny)
 
+# Rascunho ----------------------------------------------------------------
+start <- Sys.time()
+end <- Sys.time()
+time <- end - start
+as.numeric(time)
 
+# Ui ----------------------------------------------------------------------
+  
 ui <- fluidPage(
     
     
@@ -12,8 +19,9 @@ ui <- fluidPage(
             
             #  input -> botão para clicar
             actionButton(
-                inputId = "contador",
-                label = "Clique aqui"
+                inputId = "botao",
+                label = "Clique aqui",
+                class = "btn-success"
             )
             
         ),
@@ -26,16 +34,66 @@ ui <- fluidPage(
   
 )
 
+# Server ------------------------------------------------------------------
 
 server <- function(input, output, session) {
+
+  # contador para cada vez que o botão é clicado
+  contador <- reactiveVal(value = 0)
   
-    # output -> contador que recebe os cliques e atualiza a cada ação
-    output$numero <- renderText({
-        
-        input$contador
-        
-    })
-    
+  # tempo entre as contagens
+  time <- reactiveVal()
+  
+  observeEvent(
+    input$botao, {
+      
+      # # contador
+      cont <- contador() # recebe o valor reativo
+      contador(cont + 1) # atualiza pra cada vez que o botão é clicado
+      
+      # tempo
+      time(capture.output(tictoc::toc()))
+      tictoc::tic()
+      
+      # # condição
+      # if (time() < 2) {
+      #   # contador
+      #   cont <- contador() # recebe o valor reativo
+      #   contador(cont + 1) # atualiza pra cada vez que o botão é clicado
+      #   
+      # } else {
+      #     contador(NULL)
+      #     print("passei por aqui")
+      # }      
+      
+      # # condição
+      # if (time() > 2) {
+      #   contador(NULL)
+      #   print("passei por aqui")
+      # } else {
+      #   
+      #   # contador
+      #   cont <- contador() # recebe o valor reativo
+      #   contador(cont + 1) # atualiza pra cada vez que o botão é clicado
+      #   
+      # }
+    }
+  )
+  
+  # output -> contador que imprime os cliques
+  output$numero <- renderText({
+
+    paste0(
+      
+      "Quantidade de vezes que você clicou no botão:",
+      contador(),
+      
+      "Tempo entre cliques:",
+      time()
+      
+    )
+  })
+  
 }
 
 shinyApp(ui, server)
